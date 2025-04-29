a1856ae220a54f6e65c7eaa5930e082def5fb638
Julian Thatguy <jborner111@gmail.com>
2025-04-28 21:48:23 -0500

    test 20

---
+++ src/app/api/favorites/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest } from "next/server";
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+export async function GET(){
+    
+       
+    
+  try {
+    const stripe =await new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET}`)
+    if(stripe){
+        const newData=await stripe.products.list({
+            limit:100
+          })
+        return NextResponse.json({data:newData});
+    }else{
+        return NextResponse.json({data:'Incorrect Key!'})
+    }
+    
+  } catch (error) {
+    return NextResponse.json({error:error})
+  }
+   
+}
+
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,10 +1,15 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 export async function GET(){
-  const stripe=new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET}`)
-  let products=await stripe?.products?.list({
-    limit:100
-  })
-  console.log(products.data)
-  return NextResponse.json(products.data)
+ 
+  try {
+    const stripe=await new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET}`)
+    let products=await stripe?.products?.list({
+      limit:100
+    })
+    console.log(products.data)
+    return NextResponse.json({data:products.data})
+  } catch (error) {
+    return NextResponse.json({error:error})
+  }
 }
